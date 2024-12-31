aea0fb9b2c4edb980195bdb64b66fb1d2779559e
Sayam Ghosh <sayamghosh.dev@gmail.com>
2024-12-31 09:06:12 +0530

    images animation added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { useScroll, useMotionValueEvent } from "motion/react";
 
 export default function Work() {
-  const[images,setImages]=useState([
+  const [images, setImages] = useState([
     {
       url: "https://images.pexels.com/photos/3752194/pexels-photo-3752194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       top: "54%",
       left: "50%",
-      isActive: true,
+      isActive: false,
     },
     {
       url: "https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -38,8 +39,45 @@ export default function Work() {
       left: "50%",
       isActive: false,
     },
-  ])
-  
+  ]);
+
+  const { scrollY } = useScroll();
+  const { scrollYProgress } = useScroll();
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    function showImage(arr) {
+      setImages(
+        images.map((item, index) => {
+          if (arr.includes(index)) {
+            return { ...item, isActive: true };
+          } else {
+            return { ...item, isActive: false };
+          }
+        })
+      );
+    }
+    switch (Math.floor(latest * 100)) {
+      case 0:
+        showImage([]);
+        break;
+      case 1:
+        showImage([0]);
+        break;
+      case 2:
+        showImage([0,1]);
+        break;
+      case 3:
+        showImage([0,1,2]);
+        break;
+      case 4:
+        showImage([0,1,2,3]);
+        break;
+      case 5:
+        showImage([0,1,2,3,4]);
+        break;
+    }
+  });
+
   return (
     <div className="w-full mt-10">
       <div className="relative max-w-screen-xl mx-auto justify-center flex items-baseline">
@@ -54,7 +92,7 @@ export default function Work() {
           {images.map((elem, index) =>
             elem.isActive ? (
               <img
-              key={index}
+                key={index}
                 className="absolute w-64 rounded-lg -translate-x-[50%] -translate-y-[50%]"
                 style={{ top: elem.top, left: elem.left }}
                 src={elem.url}
